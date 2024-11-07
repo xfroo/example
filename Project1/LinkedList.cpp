c4565e9da5b66b5ba6980ae04f205963150beed3
שגיא עטר <saggie.attar@gmail.com>
2024-11-07 23:58:18 +0200

    Add files via upload

--- Project1/LinkedList.cpp
+++ Project1/LinkedList.cpp
@@ -1,3 +1,46 @@
 #include <iostream>
 #include "LinkedList.h"
 
+void initNode(Node* head, unsigned int number)
+{
+	head->data = number;
+	head->next = NULL;
+}
+
+void addNode(Node* head, unsigned int number)
+{
+	while (head->next != NULL)
+	{
+		head = head->next;
+	}
+
+	Node* newNode = new Node;
+	newNode->data = number;
+	newNode->next = NULL;
+	head->next = newNode;
+}
+
+
+
+void removeNode(Node* head)
+{
+	if (head == NULL)
+	{
+		std::cout << "List is empty, nothing to delete.";
+		return;
+	}
+
+	//moves head to the next node
+	Node* temp = head;
+	head = head->next;
+
+	//delete the old head node
+	delete temp;
+}
+
+void printList(Node* head)
+{
+	int i = 0;
+
+	
+}
--- Project1/LinkedList.h
+++ Project1/LinkedList.h
@@ -1,1 +1,17 @@
-#pragma once
+#ifndef LINKEDLIST_H
+#define LINKEDLIST_H
+
+
+typedef struct Node{
+	unsigned int  data;
+	Node *next;
+
+}Node;
+
+void initNode(Node* head, int number);
+void addNode(Node* head, int number);
+void removeNode(Node* head);
+void printList(Node* head);
+
+
+#endif
